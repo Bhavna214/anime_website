f71ee8f7b259fa20e0b3443c2752e9d763f08e6c
Drishti <82228411+Drishti9421@users.noreply.github.com>
2022-05-19 23:22:46 +0530

    Made path changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/header/Header';
-import Sidebar from './components/sidebar/Sidebar';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 function App() {
   return (
     <div className="app">
