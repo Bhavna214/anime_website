08134ebeb0dc5c0def8e03869cdd8bf4d90d5b5d
Bhavna214 <80099365+Bhavna214@users.noreply.github.com>
2022-05-19 23:42:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
 function App() {
   return (
     <div className="app">
