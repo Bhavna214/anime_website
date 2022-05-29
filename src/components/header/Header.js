86307c28a87a3c0ef6253c4794f07764809ee564
Parshvavyas17 <parshvavyas07@gmail.com>
2022-05-29 19:00:15 +0530

    Avatar popover created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,33 @@ import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import {Avatar, Button} from '@mui/material';
+import {Avatar, Button, makeStyles, Popover} from '@mui/material';
 import React from 'react'
 import logo from "../../assets/logo.png";
 import './styles.css'
 
+// const useStyles = makeStyles((theme)=>({
+//     large: {
+//         width: theme.spacing(7),
+//         height: theme.spacing(7)
+//     }
+// }))
+
 const Header = () => {
+    // const classes= useStyles();
+    const [anchorEl,setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    const open = Boolean(anchorEl)
+    const id = open ? "simple popover" : undefined;
+
   return (
     <div className="header">
         <div className='header__left'>
@@ -27,7 +48,23 @@ const Header = () => {
         <div className='header__right'>
             <VideoCallIcon/>
             <NotificationsIcon/>
-            <Avatar/>
+            <Avatar onClick={handleClick} />
+
+            <Popover 
+                open={open}
+                id={id}
+                onClose={handleClose}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center"
+                }}
+                transformOrigin={{
+                    vertical: "top"
+                }}
+            >
+                <h1>Hello</h1>
+            </Popover>
         </div>
     </div>
   )
