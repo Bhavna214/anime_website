db3c16f76275ff607f70f1050a71e8cc768ed121
Parshvavyas17 <parshvavyas07@gmail.com>
2022-06-01 00:23:08 +0530

    Create account page completed

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Checkbox, FormControlLabel, TextField } from '@mui/material';
 import { useState } from 'react';
-import './styles.css'
+import './styles.css';
 
 const initialFormData = {
   firstName: "",
@@ -15,6 +15,8 @@ const Signup = ({setShowSignUp}) => {
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState(initialFormData);
   const [checked, setChecked] = useState(false);
+  const [passwordError, setPasswordError] = useState({state:false, msg:''});
+  const [emailError, setEmailError] = useState({state:false, msg:''});
 
   const toggleSignUp = () => {
     setLoading(true);
@@ -24,6 +26,54 @@ const Signup = ({setShowSignUp}) => {
         setShowSignUp(false);
     },1500)
   };
+
+  const createAccount = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const err = formData.password !== formData.confirmPassword;
+    
+    if(err) {
+      setPasswordError({state:true,msg:'Passwords do not match'});
+      setFormData({...formData, confirmPassword:''});
+      setLoading(false);
+      return;
+    }
+    else {
+      setPasswordError({state:false,msg:''});
+      setEmailError({state:false,msg:''});
+    }
+
+    auth.createUserWithEmailAndPassword(formData.email,formData.password)
+    .then(() => {
+      auth.currentUser.updateProfile({
+        displayName: `${formData.firstName} ${formData.lastName}`
+      })
+      .then(() => {
+        setLoading(false);
+        setEmailError({state:false, msg:''});
+        setPasswordError({state: false, msg: ''});
+      })
+    })
+    .catch((error) => {
+      if(error.code === 'auth/email-already-in-use') {
+        setEmailError({state:true,msg:'Email is already in use'})
+        setLoading(false)
+        setFormData({...formData,email:''})
+      }
+      if(error.code === 'auth/invalid-email') {
+        setEmailError({state:true,msg:'Email is not properly formatted'})
+        setFormData({...formData,email:''})
+        setLoading(false)
+      }
+      else if(error.code === 'auth/weak-password') {
+        setPasswordError({state:true,msg:'Password should atleast be 6 characters'})
+        setFormData({...formData,password:'',confirmPassword:''})
+        setLoading(false)
+      }
+    })
+  }
+
+  const disabled = !formData.email || !formData.password || !formData.firstName || !formData.lastName || !formData.confirmPassword;
 
   return (
     <div className='signup__container'>
@@ -50,11 +100,11 @@ const Signup = ({setShowSignUp}) => {
                     lastName: e.target.value}
                   )}} />
               </div>
-              <TextField id='outlined-basic' fullWidth label='Email' type='email' variant='outlined' helperText="You can use numbers, letters and periods" value={formData.email} onChange={(e)=>{
+              <TextField id='outlined-basic' fullWidth label='Email' type='email' variant='outlined' helperText={emailError.state ? emailError.msg : "You can use numbers, letters and periods"} value={formData.email} onChange={(e)=>{
                   setFormData({
                     ...formData,
                     email: e.target.value}
-                  )}} />
+                  )}} error={emailError.state} />
 
               <div className="signup__passwordInputs">
                 <div className="signup__passwordWrapper">
@@ -62,14 +112,14 @@ const Signup = ({setShowSignUp}) => {
                   setFormData({
                     ...formData,
                     password: e.target.value}
-                  )}} />
+                  )}} error={passwordError.state} />
                 <TextField id='outlined-basic' className='signup__passwordInput' label='Confirm Password' type={checked ? 'text':'password'} variant='outlined' value={formData.confirmPassword} onChange={(e)=>{
                   setFormData({
                     ...formData,
                     confirmPassword: e.target.value}
-                  )}} />
+                  )}} error={passwordError.state} />
                 </div>
-                <p className='signup__helpertext'>You can use 8 or more characters with a mix of letters, numbers and symbols</p>
+                <p className={`signup__helpertext ${passwordError.state && "signin__error"}`}>{passwordError.state ? passwordError.msg : 'You can use 8 or more characters with a mix of letters, numbers and symbols'}</p>
 
                 <FormControlLabel control={<Checkbox checked={checked} color='primary' onClick={() => {
                   setChecked(!checked)
@@ -78,7 +128,7 @@ const Signup = ({setShowSignUp}) => {
 
               <div className='signup__buttons' >
                 <Button className='signup__button' variant='text' color='primary' onClick={toggleSignUp}>Sign In instead</Button>
-                <Button className='signup__button' variant='contained' color='primary'>Create</Button>
+                <Button className='signup__button' variant='contained' color='primary' onClick={createAccount} disabled={disabled} >Create</Button>
               </div>
             </div> 
           </div>
