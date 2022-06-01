1a4f3f39791a3ed35c1c63990903e84698d38e6e
Bhavna214 <80099365+Bhavna214@users.noreply.github.com>
2022-06-01 23:23:56 +0530

    Update SelectVideo.js

--- src/components/UploadVideo/SelectVideo.js
+++ src/components/UploadVideo/SelectVideo.js
@@ -1,67 +1,34 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
+import { TextField, Button} from '@mui/material'
 import PublishIcon from '@mui/icons-material/Publish';
 import './styles.css';
-import { TextField, Button } from '@mui/material';
-import {v4 as uuidv4} from 'uuid'
-import { useAppContext } from '../../context/appContext';
 
-const SelectVideo = () => {
+export class SelectVideo extends Component {
+    render() {
+        return (
+            <div className='SelectVideo'>
+                <div className="SelectVideo_header">
+                    <h1>UPLOAD NEW VIDEO</h1>
+                </div>
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState(0);
-    const [thumbnail, setThumbnail] = useState(null);
-    const [video, setVideo] = useState(null);
-    const [thumbnailURL, setThumbnailURL] = useState(null);
-    const [videoURL, setVideoURL] = useState(null)
+                <div className="SelectVideo_content">
+                    <PublishIcon className='SelectVideo_publishIcon' />
+                    <p>Drag and Drop the files to Upload</p>
+                    <input className='customFile_input' type='file' />
 
-    const [id, setId] = useState(uuidv4)
+                    <TextField label='Title' variant='outlined' className='SelectVideo_input' />
+                    <TextField label='Description' multiline rows={6} variant='outlined' className='SelectVideo_input' />
 
-    const [videoUploaded, setVideoUploaded]= useState(false)
-    const [thumbnailUploaded, setThumbnailUploaded] = useState(false)
+                    <input className='customFile_thumbnail' type='file'/>
 
-    const {currentUser} = useAppContext();
+                    <p className='SelectVideo_text'>By submitting your videos to Animino you acknowledge that you agree to Animino's Terms of Service and Community guidelines. Please be sure to not violate other's copyrights or Privacy rights. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, officia. At enim eveniet molestias, nostrum laborum obcaecati beatae ducimus sed magni commodi, culpa ipsum adipisci illum, blanditiis dolore minima quasi. Ex libero numquam aut recusandae</p>
 
-    const createID = () => setId(uuidv4())
+                    <Button variant='contained' className='UploadButton'>UPLOAD</Button>
 
-    const handleSubmit = () => {
-        createID();
-    }
-
-    const thumbnailChange = (e) => {
-        if(e.target.files[0]){
-            setThumbnail(e.target.files[0])
-        }
-    }
-
-    const videoChange = (e) => {
-        if(e.target.files[0]){
-            setVideo(e.target.files[0])
-        }
-    }
-
-    return (
-        <div className='SelectVideo'>
-            <div className="SelectVideo_header">
-                <h1>UPLOAD NEW VIDEO</h1>
+                </div>
             </div>
-
-            <div className="SelectVideo_content">
-                <PublishIcon className='SelectVideo_publishIcon' />
-                <p>Drag and Drop the files to Upload</p>
-                <input className='customFile_input' type='file' onChange={videoChange}/>
-
-                <TextField label='Title' variant='outlined' className='SelectVideo_input' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <TextField label='Description' multiline rows={6} variant='outlined' className='SelectVideo_input' value={description} onChange={(e)=>setDescription(e.target.value)}/>
-
-                <input className='customFile_thumbnail' type='file' onChange={thumbnailChange}/>
-
-                <p className='SelectVideo_text'>By submitting your videos to Animino you acknowledge that you agree to Animino's Terms of Service and Community guidelines. Please be sure to not violate other's copyrights or Privacy rights. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, officia. At enim eveniet molestias, nostrum laborum obcaecati beatae ducimus sed magni commodi, culpa ipsum adipisci illum, blanditiis dolore minima quasi. Ex libero numquam aut recusandae</p>
-
-                <Button variant='contained' className='UploadButton'>UPLOAD</Button>
-
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default SelectVideo
